021f4dfd2e27976966750547c88d9df6d91a7c52
ChiamatemiFrank <58424338+ChiamatemiFrank@users.noreply.github.com>
2021-01-20 11:20:35 +0100

    Add files via upload

--- EpSAutoencoder.java
+++ EpSAutoencoder.java
@@ -1,40 +1,43 @@
 package EpSAutoencoder;
 
-import org.bytedeco.javacpp.tools.Logger;
 import org.datavec.api.records.reader.RecordReader;
 import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
 import org.datavec.api.records.reader.impl.transform.TransformProcessRecordReader;
+import org.datavec.api.records.writer.impl.csv.CSVRecordWriter;
 import org.datavec.api.split.FileSplit;
+import org.datavec.api.transform.MathOp;
 import org.datavec.api.transform.TransformProcess;
 import org.datavec.api.transform.schema.Schema;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.datasets.iterator.INDArrayDataSetIterator;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.layers.DenseLayer;
 import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.transferlearning.TransferLearning;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.nd4j.common.io.ClassPathResource;
-import org.nd4j.evaluation.classification.Evaluation;
-import org.nd4j.evaluation.regression.RegressionEvaluation;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.SplitTestAndTrain;
+import org.nd4j.linalg.dataset.api.MultiDataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.dataset.api.preprocessor.NormalizerMinMaxScaler;
-import org.nd4j.linalg.dataset.api.preprocessor.NormalizerStandardize;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.learning.config.AdaGrad;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
+import org.nd4j.shade.protobuf.compiler.PluginProtos;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 public class EpSAutoencoder {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
 
         MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
@@ -44,13 +47,13 @@ public class EpSAutoencoder {
             .activation(Activation.TANH)
             .l2(0.0001)
             .list()
-            .layer(new DenseLayer.Builder().nIn(178).nOut(100)
+            .layer(new DenseLayer.Builder().nIn(179).nOut(100)
                 .build())
             .layer(new DenseLayer.Builder().nIn(100).nOut(64)
                 .build())
             .layer(new DenseLayer.Builder().nIn(64).nOut(100)
                 .build())
-            .layer(new OutputLayer.Builder().nIn(100).nOut(178)
+            .layer(new OutputLayer.Builder().nIn(100).nOut(179)
                 .activation(Activation.TANH)
                 .lossFunction(LossFunctions.LossFunction.MSE)
                 .build())
@@ -58,20 +61,21 @@ public class EpSAutoencoder {
 
 
         MultiLayerNetwork net = new MultiLayerNetwork(conf);
-        net.setListeners(Collections.singletonList(new ScoreIterationListener(10)));
+        net.setListeners(Collections.singletonList(new ScoreIterationListener(100)));
 
         //loading data from CSV with schema
         Schema schema = new Schema.Builder()
             .addColumnString("first")
-            .addColumnsInteger("int_%d",1,179)
+            .addColumnsInteger("int_%d", 1, 179)
             .build();
 
         TransformProcess transformProcess = new TransformProcess.Builder(schema)
-            .removeColumns("first","int_179").build();
+            .integerMathOp("int_179", MathOp.Subtract,1)
+            .removeColumns("first").build();
 
-        RecordReader recordReader = new CSVRecordReader(1,',');
+        RecordReader recordReader = new CSVRecordReader(1, ',');
         try {
-            recordReader.initialize(new FileSplit( new ClassPathResource("data.csv").getFile() ));
+            recordReader.initialize(new FileSplit(new ClassPathResource("data.csv").getFile()));
 
 
         } catch (IOException e) {
@@ -80,10 +84,11 @@ public class EpSAutoencoder {
             e.printStackTrace();
         }
         //Passing transformation process to convert the csv file
-        RecordReader transformProcessRecordReader = new TransformProcessRecordReader(recordReader,transformProcess);
+        RecordReader transformProcessRecordReader = new TransformProcessRecordReader(recordReader, transformProcess);
+
         //Normalizing each batch
-        NormalizerMinMaxScaler n = new NormalizerMinMaxScaler(-1,1);
-        DataSetIterator iter = new RecordReaderDataSetIterator( transformProcessRecordReader, 100  );
+        NormalizerMinMaxScaler n = new NormalizerMinMaxScaler(-1, 1);
+        DataSetIterator iter = new RecordReaderDataSetIterator(transformProcessRecordReader, 100,178,5);
         n.fit(iter);
         iter.setPreProcessor(n);
 
@@ -95,37 +100,99 @@ public class EpSAutoencoder {
 
 
         Random r = new Random(12345);
-        while(iter.hasNext()){
+        while (iter.hasNext()) {
             org.nd4j.linalg.dataset.DataSet ds = iter.next();
             SplitTestAndTrain split = ds.splitTestAndTrain(80, r);  //80/20 split (from miniBatch = 100)
             featuresTrain.add(split.getTrain().getFeatures());
             dsTest = split.getTest();
             featuresTest.add(dsTest.getFeatures());
-            INDArray indexes = Nd4j.argMax(dsTest.getLabels(),1); //Convert from one-hot representation -> index
-            labelsTest.add(indexes);
+            labelsTest.add(ds.getLabels());
         }
 
         //Training the model:
-        int nEpochs = 30;
-        for( int epoch=0; epoch<nEpochs; epoch++ ){
-            for(INDArray data : featuresTrain){
-                net.fit( data,data);
+        int nEpochs1 = 15;
+        for (int epoch = 0; epoch < nEpochs1; epoch++) {
+            for (INDArray data : featuresTrain) {
+                net.fit(data, data);
 
             }
             System.out.println("Epoch " + epoch + " complete");
         }
 
+
+
+        System.out.println(net.summary());
+
+        //Layer reduction and new DataSet after features reduction
+        MultiLayerNetwork redNet = new TransferLearning.Builder(net)
+            .removeLayersFromOutput(2)
+            .setFeatureExtractor(1)
+            .build();
+        System.out.println(redNet.summary());
+
+
+        List<INDArray> reducedFeatures = new ArrayList<>();
+        List<INDArray> newLabels =new ArrayList<>();
+        List<INDArray> newInputs = new ArrayList<>();
+        //CSVRecordWriter recordWriter = new CSVRecordWriter();
+        // recordWriter.iniz
+
+
+        while (iter.hasNext()){
+            org.nd4j.linalg.dataset.DataSet ds = iter.next();
+            reducedFeatures.add(ds.getFeatures());
+            newLabels.add(ds.getLabels());
         }
 
+        for (INDArray redData : reducedFeatures){
+            newInputs = redNet.feedForward(redData);
+        }
 
+        Iterator inputsIter = newInputs.iterator();
+        Iterator labelsIter = newLabels.iterator();
 
+      //  while (outputIter.hasNext()){
+        //    recordWriter.write(outputIter.next().toString());
+        }
 
+        //Inizializing classifier
+        MultiLayerConfiguration classifierConfig = new NeuralNetConfiguration.Builder()
+            .seed(12345)
+            .weightInit(WeightInit.XAVIER)
+            .updater(new AdaGrad(0.05))
+            .activation(Activation.RELU)
+            .l2(0.0001)
+            .list()
+            .layer(new DenseLayer.Builder().nIn(64).nOut(32)
+                .build())
+            .layer(new DenseLayer.Builder().nIn(32).nOut(16)
+                .build())
+            .layer(new DenseLayer.Builder().nIn(16).nOut(8)
+                .build())
+            .layer(new OutputLayer.Builder().nIn(8).nOut(2)
+                .activation(Activation.SOFTMAX)
+                .lossFunction(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                .build())
+            .build();
 
+        MultiLayerNetwork classifier = new MultiLayerNetwork(classifierConfig);
+
+        int nEpochs2=30;
+
+      /*  for (int j=0; j< nEpochs2; j++){
+            while(inputsIter.hasNext() && labelsIter.hasNext()){
+            classifier.fit( (INDArray) inputsIter.next(), (INDArray) labelsIter.next());
+            classifier.score();
+            }
+        }
+
+*/
 
 
 
 
     }
+
 
 
 
